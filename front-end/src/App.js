9f36b7df3739dbfb260f2a30ea7ca2c177c82055
isobelm <isobel.mahon@gmail.com>
2019-11-01 20:12:50 +0000

    v basic homepage

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import HomePage from "./HomePage";
 
 function App() {
-  return (
-    <div className="App">
-      Bare
-    </div>
-  );
+	return (
+		<div className="App">
+			<HomePage />
+		</div>
+	);
 }
 
 export default App;
