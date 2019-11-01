909a7e9f26bc4f4cb0e93215b3cc34f9ee61d4e5
ebinmoolan@gmail.com <bennye@tcd.ie>
2019-11-01 20:22:04 +0000

    Routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import HomePage from "./HomePage";
 
 function App() {
   return (
     <Router>
       <Route exact path="/" component={HomePage} />
-      <Route path="/:questionnaire" component={HomePage} />
+      <Route path="/:questionnaireType" component={HomePage} />
     </Router>
   );
 }
