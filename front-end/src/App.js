ce032c41e91e33856ec192d02a9d80cdfadc890f
isobelm <isobel.mahon@gmail.com>
2019-11-01 23:22:20 +0000

    route to text page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import HomePage from "./Components/HomePage";
 import ScalePage from "./Components/ScalePage";
+import TextPage from "./Components/TextPage";
 
 function App() {
 	return (
 		<Router>
 			<Route exact path="/" component={HomePage} />
+			<Route path="/adjustments/:userID" component={TextPage} />
 			<Route path="/:questionnaireType/:userID" component={ScalePage} />
 		</Router>
 	);
