35cead629029ed0b23e6083ff589fdd8a1662465
hogand7 <hogand7@tcd.ie>
2019-11-01 21:44:34 +0000

    Who needs testing? #MindTheGap

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,1 +1,8 @@
-console.log("Test");
+const router = express.Router();
+
+router.get("/getHistroy/:username", async (req, res) => {
+    var workbook = XLSX.readFile('file://C:/Users/Donal/Downloads/wpforms-Autistica-8211-Adjustments-10-29-2019.xlsx');
+    var csv = XLSX.utils.sheet_to_csv(workbook.sheets[0]);
+    var data = $.csv.toObjects(csv);
+    return data;
+});
