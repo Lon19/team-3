70c6a979eaa44f49475be515e23def8e6fe48135
hogand7 <hogand7@tcd.ie>
2019-11-01 22:18:00 +0000

    I'll optimise this code at somepoint. Somepoint. #DarkFate

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,8 +1,35 @@
 const router = express.Router();
 
 router.get("/getHistroy/:username", async (req, res) => {
-    var workbook = XLSX.readFile('file://C:/Users/Donal/Downloads/wpforms-Autistica-8211-Adjustments-10-29-2019.xlsx');
+    var workbook = XLSX.readFile('wpforms-Autistica-8211-Mental-Health.xlsx');
     var csv = XLSX.utils.sheet_to_csv(workbook.sheets[0]);
     var data = $.csv.toObjects(csv);
-    return data;
+    var res = [];
+    var index = 0;
+
+    for(X in data){
+        if(X.Username == username){
+            var sum = 0; 
+            for(Y in X){
+                switch(Y){
+                    case "strongly disagree":
+                        sum += 1;
+                        break;
+                    case "somewhat disagree":
+                        sum += 2;
+                        break;
+                    case "somewhat agree":
+                        sum += 3;
+                        break;
+                    case "strongly agree":
+                        sum += 4;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            res[index++] = sum;
+        }
+    }
+    return res;
 });
