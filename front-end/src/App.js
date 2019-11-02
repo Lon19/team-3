18164b67b9a903b806f8179e42d656aa844ab6d3
isobelm <isobel.mahon@gmail.com>
2019-11-02 09:41:02 +0000

    unfinished settings

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,12 +5,15 @@ import HomePage from "./Components/HomePage";
 import SettingsPage from "./Components/SettingsPage";
 import ScalePage from "./Components/ScalePage";
 import TextPage from "./Components/TextPage";
+import { StoreValue } from "./Services/Cookies";
 
 function App() {
+	StoreValue("font-size", 20);
+
 	return (
 		<Router>
 			<Switch>
-				<Route exact path="/" component={HomePage} />
+				<Route exact path="/" component={SettingsPage} />
 				<Route
 					path="/:questionnaireType/:userID/:date"
 					component={TextPage}
