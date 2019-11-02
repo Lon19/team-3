a38ed5b2b4d9f19c30fd6685f654aea2a81c4c30
hogand7 <hogand7@tcd.ie>
2019-11-02 02:18:21 +0000

    I've finally done enough to sleep! #bugKillOfTheWeek

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,38 +1,48 @@
 var express = require('express')
 var router = express()
+const API_PORT = 3000;
+const app = express();
 
 XLSX = require('xlsx');
 csv = require('jquery-csv');
 
-var workbook = XLSX.readFile(require('path').resolve(__dirname, 'wpforms-Autistica-8211-Mental-Health.xlsx'));
+var workbook = XLSX.readFile(require('path').resolve(__dirname, 'wpforms-Autistica-8211-Organisational-Culture.xlsx'));
 var csvMental = XLSX.utils.sheet_to_csv(workbook.Sheets[workbook.SheetNames[0]]);
 var dataMental = csv.toObjects(csvMental);
 
-router.get("/getHistory/:username", (req, res) => {
-    var res = [];
-    for(X in dataMental){
-        if(X.Username == username){
-            var sum = 0; 
-            for(Y in X){
-                switch(Y){
-                    case "strongly disagree":
-                        sum += 1;
-                        break;
-                    case "somewhat disagree":
-                        sum += 2;
-                        break;
-                    case "somewhat agree":
-                        sum += 3;
-                        break;
-                    case "strongly agree":
-                        sum += 4;
-                        break;
-                    default:
-                        break;
-                }
+function makeNum(response){
+    switch(response){
+        case "Strongly disagree":
+            return 1;
+        case "Somewhat disagree":
+            return 2;
+        case "Somewhat agree":
+            return ;
+        case "Strongly agree":
+            return 1;
+        default:
+            return 0;
+    }
+}
+
+router.get("/getHistory/:userid", (req, res) => {
+    const userid = req.params.userid;
+    var result = [];
+    for(i = 0; i < dataMental.length; i++){
+        if(dataMental[i].Username === userid){
+            var sum = 0;
+            for(X in dataMental[i]){
+                console.log(dataMental[i][X]);
+                sum += makeNum(dataMental[i][X]);
             }
-            res.push = {date: X.Date, number: sum};
+            sum = sum/7;
+            result.push({date: dataMental[i].Date, number: sum});
         }
     }
-    return res;
+    return res.json(result);
 });
+
+app.use("/", router);
+app.disable("etag");
+
+app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}/`));
