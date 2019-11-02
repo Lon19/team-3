ee53d4d5d9fbafb51313850b74d10cd2a83c9483
ebinmoolan@gmail.com <bennye@tcd.ie>
2019-11-02 07:26:46 +0000

    Backend linking stuff

--- front-end/src/Components/TextPage.js
+++ front-end/src/Components/TextPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Header from "./Header";
 import { MapType } from "../Services/QuestionnaireTypes";
 import HorizontalTimeline from "react-horizontal-timeline";
+import { getHistory } from "../Services/Requests";
 
 class TextPage extends Component {
 	constructor(props) {
@@ -10,14 +11,36 @@ class TextPage extends Component {
 		this.state = {
 			type: MapType(decodeURIComponent(this.props.match.params.questionnaireType)),
 			userID: decodeURIComponent(this.props.match.params.userID),
-			value: 0,
+			date: undefined,
 			previous: 0,
-			data: [{ question: "Was you sad today?", answer: "I was." }, { question: "Was you sad today?", answer: "I was." }]
+			dataMap: [{ question: "Was you sad today?", answer: "I was." }, { question: "Was you sad today?", answer: "I was." }]
 		};
 
 		if (!this.state.type) {
 			this.state.type = "Adjustments";
 		}
+	}
+
+	componentDidMount() {
+		this.requestData();
+	}
+
+	componentWillReceiveProps(props, state) {
+		if (
+			props.match.params.questionnaireType !== state.type &&
+			props.match.params.userID !== state.userID &&
+			props.match.params.date !== state.date
+		) {
+			this.requestData();
+		}
+		this.setState({
+			type: MapType(
+				decodeURIComponent(props.match.params.questionnaireType)
+			),
+			user: decodeURIComponent(props.match.params.userID),
+			date: decodeURIComponent(props.match.params.date),
+			loading: true,
+		});
 	}
 
 	VALUES = ["1/2/19", "2/2/19", "3/2/19"];
@@ -48,7 +71,7 @@ class TextPage extends Component {
 						/>
 					</div>
 					<div className="textPage-questions">
-						{this.state.data.map((question) =>
+						{this.state.dataMap.map((question) =>
 							<div className="textPage-box">
 								<div className="textPage-question">{question.question}</div>
 								<div className="textPage-answer">{question.answer}</div>
@@ -57,6 +80,27 @@ class TextPage extends Component {
 				</div>
 			</div>
 		);
+	}
+
+	async requestData() {
+		let dataMap = {};
+		let values = [];
+		const data = await getHistory(this.state.userID);
+
+		if (!data) {
+			return;
+		}
+
+		for (let datum of data) {
+			dataMap[datum.date] = datum;
+			values.push(datum)
+		}
+
+		this.setState({
+			dataMap,
+			loading: false,
+			values,
+		});
 	}
 }
 
