895ad419a6ed9a14d3ca4a1c05b83e35e24ea446
Gemma Farrell <44396878+GemmaFarrellCS@users.noreply.github.com>
2019-11-02 10:30:53 +0000

    explanation paragraph

--- front-end/src/Components/HomePage.js
+++ front-end/src/Components/HomePage.js
@@ -14,6 +14,13 @@ class HomePage extends Component {
 		return (
 			<div>
 				<Header title={"DARE Progress"} nobutton={true} />
+
+				<p className="HomePage-paragraph">
+					Welcome to your progress menu. <br></br>
+					Click on one of the buttons below to view graphs of your previous answers to the questionnaires
+				</p>
+
+
 				<button
 					className="HomePage-button"
 					onClick={() => this.handleClick("Mental Health")}
